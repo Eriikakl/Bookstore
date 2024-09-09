44da27a139b63fd566d372073e720a9333602759
Eriikakl <eriika.klenberg@myy.haaga-helia.fi>
2024-09-09 19:24:57 +0300

    pohja luotu

---
+++ src/main/java/hh/sof03/bookstore/domain/Book.java
@@ -0,0 +1,77 @@
+package hh.sof03.bookstore.domain;
+
+public class Book {
+
+    private String title;
+    private String author;
+    private Integer publicationYear;
+    private String isbn;
+    private Float price;
+
+    public Book(String title, String author, Integer publicationYear, String isbn, Float price) {
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+        this.isbn = isbn;
+        this.price = price;
+    }
+
+    public Book() {
+        this.title = null;
+        this.author = null;
+        this.publicationYear = null;
+        this.isbn = null;
+        this.price = null;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Integer getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(Integer publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Book [title=" + title + ", author=" + author + ", publicationYear=" + publicationYear + ", isbn=" + isbn
+                + ", price=" + price + "]";
+    }
+
+    
+
+    
+    
+}
