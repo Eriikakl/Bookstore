a540eebc5513593385cb6c4c809b9afe30e7e4ce
Eriikakl <eriika.klenberg@myy.haaga-helia.fi>
2024-09-14 15:45:31 +0300

    Delete and add added

--- src/main/java/hh/sof03/bookstore/web/BookController.java
+++ src/main/java/hh/sof03/bookstore/web/BookController.java
@@ -3,9 +3,11 @@ package hh.sof03.bookstore.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import hh.sof03.bookstore.domain.Book;
 import hh.sof03.bookstore.domain.BookRepository;
 
 
@@ -28,6 +30,22 @@ public class BookController {
         model.addAttribute("books", repository.findAll());
         return "booklist";
     }  
+    @RequestMapping(value="/add")
+    public String addBook(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+    @RequestMapping(value="/save", method = RequestMethod.POST)
+    public String saveBook(Book book) {
+        repository.save(book);
+        return "redirect:booklist";
+    }
+
+    @RequestMapping(value="/delete/{id}", method = RequestMethod.GET)
+    public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+        repository.deleteById(bookId);
+        return "redirect:../booklist";
+    }
     
 
 }
